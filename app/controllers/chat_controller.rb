80cff39750a1e89588b499378448c4b010ce501c
Steve Coast <123912+SteveC@users.noreply.github.com>
2022-03-05 12:31:46 -0700

    Update chat_controller.rb

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,6 +1,8 @@
 class ChatController < ApplicationController
+  # make sure the user is logged in
   require_authentication!
   
+  # redirect to the correct page
   def show
     redirect_to language_messages_path(current_user.languages.to_a.first&.code || 'en')
   end
