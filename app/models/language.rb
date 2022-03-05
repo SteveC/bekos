7a91e45d43b1cea772b4e6eff5e9e9979b1a10e1
Steve Coast <123912+SteveC@users.noreply.github.com>
2022-03-05 12:36:42 -0700

    Update language.rb

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,5 @@
 class Language < ApplicationRecord
+  # there are many languages which each have a thread and a language code like en for English
   has_one :thread, foreign_key: 'language_code', class_name: 'ChatThread'
   
   def thread! = thread || create_thread
