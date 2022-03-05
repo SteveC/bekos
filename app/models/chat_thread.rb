eb88a0e83a31897ee5992ab8cfbda045ab1599b3
Steve Coast <123912+SteveC@users.noreply.github.com>
2022-03-05 12:36:05 -0700

    Update chat_thread.rb

--- app/models/chat_thread.rb
+++ app/models/chat_thread.rb
@@ -1,9 +1,12 @@
 class ChatThread < ApplicationRecord
+  
+  # each chat thread belongs to a language (french, german....) and has many messages within it
   belongs_to :language, foreign_key: 'language_code', optional: true
   has_many :messages, foreign_key: 'thread_id', class_name: 'ChatMessage'
   
   scope :with_users, -> user_1, user_2 { where "user_ids @> array[?]::bigint[]", [user_1, user_2] }
   
+  # if the users dont exist make sure they get created
   class << self
     def with_users!(user_1, user_2) = with_users(user_1, user_2) || create!(user_ids: [user_1, user_2])
   end
