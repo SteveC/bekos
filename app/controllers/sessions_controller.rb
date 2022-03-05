bc221e88ef48aa7c892226417c073383f214f147
Steve Coast <123912+SteveC@users.noreply.github.com>
2022-03-05 12:33:04 -0700

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
+  # manage user sessions
+  
   def show
   end
   
+  # create a new session
   def create
     @user = User.authenticate!(params[:login], params[:password])
     session[:user_id] = @user.id
@@ -12,6 +15,7 @@ class SessionsController < ApplicationController
     render :show
   end
   
+  # remove a session (basically, log out)
   def destroy
     session[:user_id] = nil
     redirect_to login_path
